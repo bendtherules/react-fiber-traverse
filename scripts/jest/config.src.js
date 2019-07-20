7b53266634e5b936be8c987dbe52d166742116f2
bendtherules <abhasbhattacharya2@gmail.com>
2019-07-20 18:47:51 +0530

    Revert "Add 'jest-plugin-must-assert' package"
    This reverts commit d939bde9f7c0ec1cd2e60a7133f3aecdad8facbe.

--- scripts/jest/config.src.js
+++ scripts/jest/config.src.js
@@ -6,7 +6,7 @@ module.exports = {
   rootDir: process.cwd(),
   roots: ['<rootDir>/test'],
   setupFiles: ['<rootDir>/scripts/jest/setupEnvironment.ts'],
-  setupFilesAfterEnv: ['<rootDir>/scripts/jest/setupJest.ts', 'jest-plugin-must-assert'],
+  setupFilesAfterEnv: ['<rootDir>/scripts/jest/setupJest.ts'],
   testMatch: ['<rootDir>/test/*.spec.ts?(x)'],
   transform: { '^.+\\.(js|tsx?)$': 'ts-jest' }
 }
