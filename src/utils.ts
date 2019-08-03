ff9821329ab47cb0c59ca2cecb5c4b998ef5a600
bendtherules <abhasbhattacharya2@gmail.com>
2019-08-03 18:48:01 +0530

    src/utils - Add helpers for detecting react functional and class components

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,69 @@
-import * as React from 'react';
-import { FiberNode, FiberNodeisHTMLLike, FiberNodeForFunctionComponent, FiberNodeForComponentClass } from './mocked-types';
+import * as React from "react";
+import {
+  FiberNode,
+  FiberNodeisHTMLLike,
+  FiberNodeForFunctionComponent,
+  FiberNodeForComponentClass
+} from "./mocked-types";
 
 function isNodeHtmlLike(node: FiberNode): node is FiberNodeisHTMLLike {
-    return (typeof node.type === "string") || node.type === null;
+  return typeof node.type === "string" || node.type === null;
 }
 
-function isNodeNotHtmlLike(node: FiberNode): node is Exclude<FiberNode, FiberNodeisHTMLLike> {
-    return !isNodeHtmlLike(node);
+function isNodeNotHtmlLike(
+  node: FiberNode
+): node is Exclude<FiberNode, FiberNodeisHTMLLike> {
+  return !isNodeHtmlLike(node);
 }
 
-function isNodeFunctionComponent(node: FiberNode): node is FiberNodeForFunctionComponent {
-    return isNodeNotHtmlLike(node) && node.stateNode === null;
+function isNodeFunctionComponent(
+  node: FiberNode
+): node is FiberNodeForFunctionComponent {
+  return isNodeNotHtmlLike(node) && node.stateNode === null;
 }
 
-function isNodeComponentClass(node: FiberNode): node is FiberNodeForComponentClass {
-    return isNodeNotHtmlLike(node) && (node.stateNode instanceof React.Component);
+function isNodeComponentClass(
+  node: FiberNode
+): node is FiberNodeForComponentClass {
+  return isNodeNotHtmlLike(node) && node.stateNode instanceof React.Component;
 }
 
-export { isNodeHtmlLike, isNodeNotHtmlLike, isNodeFunctionComponent, isNodeComponentClass };
+function isConstructorHtmlLike(ctr: React.ElementType): ctr is Exclude<React.ElementType, React.ComponentType> {
+  if (typeof ctr === "string" || ctr === null) {
+    return true;
+  }
+  return false;
+}
+
+function isConstructorComponentClass(
+  ctr: React.ElementType
+): ctr is React.ComponentClass {
+  if (isConstructorHtmlLike(ctr)) {
+    return false;
+  }
+
+  if (
+    ctr.prototype !== undefined &&
+    (ctr.prototype as any) instanceof React.Component
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+function isConstructorFunctionComponent(
+  ctr: React.ElementType
+): ctr is React.FunctionComponent {
+  return !isConstructorComponentClass(ctr);
+}
+
+export {
+  isNodeHtmlLike,
+  isNodeNotHtmlLike,
+  isNodeFunctionComponent,
+  isNodeComponentClass,
+  isConstructorHtmlLike,
+  isConstructorComponentClass,
+  isConstructorFunctionComponent
+};
