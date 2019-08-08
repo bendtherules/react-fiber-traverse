28d5f6ab1d12faa462f10d5542a5b1d362f793cb
bendtherules <abhasbhattacharya2@gmail.com>
2019-08-08 18:30:51 +0530

    Fix jest and other test configs from build

--- scripts/jest/config.es.js
+++ scripts/jest/config.es.js
@@ -3,6 +3,6 @@ const srcConfig = require("./config.src");
 module.exports = Object.assign({}, srcConfig, {
   collectCoverage: false,
   moduleNameMapper: {
-    "^../src$": `<rootDir>/dist/react-fiber-traverse.esm.js`
+    "^../src$": `<rootDir>/dist/esm/index.js`
   }
 });
