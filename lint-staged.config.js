400757182fb039ae4dae3de7a10c6652f785d5cd
bendtherules <abhasbhattacharya2@gmail.com>
2019-08-10 13:46:21 +0530

    Modify lint-staged - Run prettier and eslint only on changed files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx}": () => "run-s check:* lint"
+  "*.{js,jsx,ts,tsx}": filenames => {
+    // Escape filenames and join by space
+    const joinedFilenames = filenames.map(str => `"${str}"`).join(" ");
+    return [
+      "run-s check:!(format)",
+      `npm run check:format -- ${joinedFilenames}`,
+      `npm run _lint -- ${joinedFilenames}`
+    ];
+    // `run-s check:!(format) "check:format -- ${joinedFilenames}" "_lint -- ${joinedFilenames}"`;
+  }
 };
